bd9f8ec9674e7e24c209305d907f5e272a8c0ff0
Brandon Meyer <brandon.meyer@my.southeastmn.edu>
2016-09-05 16:38:21 -0500

    Added ex1e

---
+++ bmeyer1e1/Main Form.vb
@@ -0,0 +1,28 @@
+﻿' Name:                         Property Tax
+' Purpose:                      Calculates the property tax
+' Programmer:                   Brandon Meyer on 9/5/16
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        'calculate the property tax
+
+        lblTax.Text = Val(txtAssessed.Text) * 0.015
+        lblTax.Text = Format(lblTax.Text, "currency")
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'prepare screen for next property tax
+        lblTax.Text = String.Empty
+        txtAssessed.Text = String.Empty
+        'send the focus to the Assessed box
+        txtAssessed.Focus()
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
